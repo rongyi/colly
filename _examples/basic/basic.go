20e67c93aa6606b9c1cd4859d773118b07b3d3d8
rongyi <sqrongyi@163.com>
2018-01-15 09:25:52 +0800

    Update basic

--- _examples/basic/basic.go
+++ _examples/basic/basic.go
@@ -28,6 +28,23 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	// If we get error just after the request, this callback will be called
+	// and colly will return at this point, other callbacks will not be called
+	c.OnError(func (resp *colly.Response, e error) {
+		fmt.Println(e.Error())
+	})
+
+	// After a success response, this callback will be called
+	c.OnResponse(func (resp *colly.Response) {
+		fmt.Printf("response with status code: %d\n", resp.StatusCode)
+	})
+
+	// this is the final callback, we usually doing some teardown jobs
+	// e.g. write to file, report to api...
+	c.OnScraped(func (resp *colly.Response) {
+		fmt.Printf("crawling url: %s done\n", resp.Request.URL.String())
+	})
+
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://hackerspaces.org/")
 }
